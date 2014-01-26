3447ac63423f03f7b13dc78481c937a3b6635469
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-01-26 22:42:09 +0100

    Adds preview

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -1,26 +1,27 @@
 (ns wordsmith.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [markdown.core :as md]))
+            [markdown.core :as md]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
 (def app-state (atom {:text "Hello world!"}))
 
 (defn handle-keydown [e app owner]
-  (let [t (om/get-node owner "input-area")
+  (let [i (om/get-node owner "input-area")
         o (om/get-node owner "output-area")
-        md (.-textContent t)]
-    (set! (.-textContent o) md)))
+        t (.-value i)
+        md (md/mdToHtml (str t "\n"))]
+    (set! (.-innerHTML o) md)))
 
 (defn wordsmith-app [app owner]
   (reify
     om/IRender
     (render [_]
       (dom/div #js {:className "container"}
-        (dom/div #js {:contentEditable true
-                      :ref "input-area"
-                      :onKeyUp #(handle-keydown % app owner)})
+        (dom/textarea #js {:ref "input-area"
+                           :onKeyUp #(handle-keydown % app owner)})
         (dom/div #js {:ref "output-area"})))))
 
 (om/root
--- wordsmith.js
+++ wordsmith.js
@@ -5,4 +5,4 @@ goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom']);
 goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
 goog.addDependency("../markdown/transformers.js", ['markdown.transformers'], ['cljs.core', 'clojure.string']);
 goog.addDependency("../markdown/core.js", ['markdown.core'], ['cljs.core', 'markdown.transformers']);
-goog.addDependency("../wordsmith/core.js", ['wordsmith.core'], ['cljs.core', 'om.core', 'markdown.core', 'om.dom']);
+goog.addDependency("../wordsmith/core.js", ['wordsmith.core'], ['cljs.core', 'om.core', 'clojure.string', 'markdown.core', 'om.dom']);
