8f8027cdb26c9b606acf8bd8dc76737e81f112c9
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-01-28 21:17:51 +0100

    Adds `marked` for markdown to html generation

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -1,8 +1,6 @@
 (ns wordsmith.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [markdown.core :as md]
-            [clojure.string :as string]))
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -12,7 +10,7 @@
   (let [i (om/get-node owner "input-area")
         o (om/get-node owner "output-area")
         t (.-value i)
-        md (md/mdToHtml (str t "\n"))]
+        md (.marked js/window (str t "\n"))]
     (set! (.-innerHTML o) md)))
 
 (defn wordsmith-app [app owner]
