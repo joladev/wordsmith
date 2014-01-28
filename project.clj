7ff2115243643f5c3d51a254512dc3e15040c003
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-01-28 21:16:19 +0100

    Removes markdown dependency

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.2.3"]
-                 [com.facebook/react "0.8.0.1"]
-                 [markdown-clj "0.9.41"]]
+                 [com.facebook/react "0.8.0.1"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
