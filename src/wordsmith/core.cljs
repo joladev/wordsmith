9fbb7b985ad98f748cb811de46d355f4aa041b8b
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-03-30 15:47:09 +0200

    Uses RenderState instead of accessing dom ref for input

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -7,19 +7,22 @@
 (def app-state (atom {}))
 
 (defn handle-input [event app owner]
-  (let [input-area  (om/get-node owner "input-area")
-        output-area (om/get-node owner "output-area")
-        text        (.-value input-area)
-        markdown    (.marked js/window text)]
-    (set! (.-innerHTML output-area) markdown)))
+  (om/set-state! owner :input (.. event -target -value))
+  (let [input (om/get-state owner :input)
+        output (.marked js/window input)
+        output-area (om/get-node owner "output-area")]
+    (set! (.-innerHTML output-area) output)))
 
 (defn wordsmith-app [app owner]
   (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
+      {:input ""})
+    om/IRenderState
+    (render-state [_ {:keys [input]}]
       (dom/div #js {:className "container"}
-        (dom/textarea #js {:ref "input-area"
-                           :onInput #(handle-input % app owner)})
+        (dom/textarea #js {:onInput #(handle-input % app owner)
+                           :value input})
         (dom/div #js {:ref "output-area"})))))
 
 (om/root
