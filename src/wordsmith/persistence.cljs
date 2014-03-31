4ad4f77667ada68ad95d14999a6f391b80b53dba
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-03-31 19:04:22 +0200

    Adds cljs semi-persistence using localStorage

---
+++ src/wordsmith/persistence.cljs
@@ -0,0 +1,21 @@
+(ns wordsmith.persistence)
+
+(def prefix "wordsmith:documents:")
+
+(def prefix-length (count prefix))
+
+(defn generate-key [title]
+  (str prefix title))
+
+(defn set-document [title content]
+  (let [k (generate-key title)]
+    (.setItem js/localStorage k content)))
+
+(defn get-document [title]
+  (let [k (generate-key title)]
+    (.getItem js/localStorage k)))
+
+(defn get-all-titles []
+  (let [ks (.keys js/Object js/localStorage)
+        filtered (filter #(= (.substring % 0 prefix-length) prefix) ks)]
+    (mapv #(.substring % prefix-length) filtered)))
