f4d71786cf29cc4fb953cf588db8e87ae28265dd
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-03-31 22:37:51 +0200

    Refactors editor into own namespace

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -1,18 +1,12 @@
 (ns wordsmith.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [wordsmith.persistence :as p]))
+            [wordsmith.persistence :as p]
+            [wordsmith.editor :as e]))
 
 (enable-console-print!)
 
 (def app-state (atom {}))
-
-(defn handle-input [event owner]
-  (let [input (.. event -target -value)
-        output (.marked js/window input)
-        output-area (om/get-node owner "output-area")]
-    (om/set-state! owner :input (.. event -target -value))
-    (set! (.-innerHTML output-area) output)))
 
 (defn handle-title-change [event owner]
   (let [title (.. event -target -value)
@@ -20,40 +14,45 @@
     (when-not (= "" title)
       (om/set-state! owner :title title)
       (p/remove-document old-title)
-      (p/set-document title (om/get-state owner :input)))))
+      (p/set-document title (om/get-state owner :input))
+      (om/transact! owner :titles #(replace {old-title title} %)))))
 
-(defn menu [app owner]
+(defn title-field [app owner]
   (reify
     om/IInitState
     (init-state [_]
       {:title ""})
     om/IRenderState
     (render-state [_ {:keys [title]}]
-      (dom/div #js {:id "wordsmith-menu"}
+      (dom/div #js {:id "title-field"}
        (dom/input #js {:type "text" :onBlur #(handle-title-change % owner)}
                   title)))))
 
-(defn markdown-field [app owner]
+(defn update-current [event owner]
+  (om/set-state! owner :current (.. event -target -text)))
+
+(defn left-menu [app owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      (:input ""))
     om/IRenderState
-    (render-state [_ {:keys [input]}]
-      (dom/div nil (.marked js/window input)))))
+    (render-state [_ {:keys [titles]}]
+      (dom/div #js {:id "left-menu"}
+        (dom/p nil (str titles))
+        (apply dom/ul nil
+          (map #(dom/li 
+                  #js {:onClick (fn [e] (update-current e owner))} %)
+               titles))))))
 
 (defn wordsmith-app [app owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:input ""})
+      {:input "" :titles (p/get-all-titles)})
     om/IRenderState
     (render-state [_ state]
-      (dom/div nil
-        (om/build menu app {:init-state state})
-        (dom/div #js {:className "container"}
-          (dom/textarea #js {:onInput #(handle-input % owner)})
-          (dom/div #js {:ref "output-area"}))))))
+      (dom/div #js {:className "container"}
+        (om/build title-field app {:init-state state})
+        (om/build left-menu app {:init-state state})
+        (om/build e/editor app {:init-state state})))))
 
 (om/root
   app-state
