1a3ec45442ed53169ed57a7662694fd931014d7e
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-03-31 20:52:35 +0200

    Adds naive saving of documents

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -1,6 +1,7 @@
 (ns wordsmith.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [wordsmith.persistence :as p]))
 
 (enable-console-print!)
 
@@ -13,19 +14,48 @@
     (om/set-state! owner :input (.. event -target -value))
     (set! (.-innerHTML output-area) output)))
 
+(defn handle-title-change [event owner]
+  (let [title (.. event -target -value)
+        old-title (om/get-state owner :title)]
+    (when-not (= "" title)
+      (om/set-state! owner :title title)
+      (p/remove-document old-title)
+      (p/set-document title (om/get-state owner :input)))))
+
+(defn menu [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:title ""})
+    om/IRenderState
+    (render-state [_ {:keys [title]}]
+      (dom/div #js {:id "wordsmith-menu"}
+       (dom/input #js {:type "text" :onBlur #(handle-title-change % owner)}
+                  title)))))
+
+(defn markdown-field [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      (:input ""))
+    om/IRenderState
+    (render-state [_ {:keys [input]}]
+      (dom/div nil (.marked js/window input)))))
+
 (defn wordsmith-app [app owner]
   (reify
     om/IInitState
     (init-state [_]
       {:input ""})
     om/IRenderState
-    (render-state [_ _]
-      (dom/div #js {:className "container"}
-        (dom/textarea #js {:onInput #(handle-input % owner)
-                           :value input})
-        (dom/div #js {:ref "output-area"})))))
+    (render-state [_ state]
+      (dom/div nil
+        (om/build menu app {:init-state state})
+        (dom/div #js {:className "container"}
+          (dom/textarea #js {:onInput #(handle-input % owner)})
+          (dom/div #js {:ref "output-area"}))))))
 
 (om/root
   app-state
-  wordsmith-app  
+  wordsmith-app
   (. js/document (getElementById "app")))
