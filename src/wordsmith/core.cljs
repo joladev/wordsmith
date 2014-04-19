fbe6d722467804bcef3ff554acb895571b61a1f1
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-04-19 17:05:55 +0200

    Fixes a few bugs from the transition

--- src/wordsmith/core.cljs
+++ src/wordsmith/core.cljs
@@ -10,34 +10,37 @@
   ICloneable
   (-clone [s] (js/String. s)))
 
-(def app-state (atom {:input "" :titles [] :current ""}))
+(def app-state (atom {:input "" :titles [] :current "" :title ""}))
 
 (defn handle-title-change [event app]
-  (let [new-title (.. event -target -value)
-        old-title (:title app)]
+  (let [new-title (.. event -target -value)]
     (when-not (= "" new-title)
-      (om/update! (:title app) :title new-title))))
+      (om/update! app :title new-title))))
 
 (defn title-field [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div #js {:id "title-field"}
-       (dom/input #js {:type "text" :onBlur #(handle-title-change % app)}
-                  (:current app))))))
+      (let [title (:title app)]
+        (dom/div #js {:id "title-field"}
+          (dom/input #js {:type "text" :onBlur #(handle-title-change % title)}
+                     (:current app)))))))
 
 (defn update-current [event app]
-  (om/update! (:current app) :current (.. event -target -text)))
+  (om/update! app :current (.. event -target -textContent)))
 
 (defn left-menu [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div #js {:id "left-menu"}
-        (apply dom/ul nil
-          (map #(dom/li 
-                  #js {:key (str %) :onClick (fn [e] (update-current e app))} %)
-               (:titles app)))))))
+      (let [current (:current app)]
+        (dom/div #js {:id "left-menu"}
+          (apply dom/ul nil
+            (map #(dom/li 
+                    #js {:key (str %) 
+                         :onClick (fn [e] (update-current e current))}
+                    (om/value %))
+                 (:titles app))))))))
 
 (defn wordsmith-app [app owner]
   (reify
@@ -46,6 +49,7 @@
       (om/update! (:titles app) :titles (p/get-all-titles)))
     om/IRender
     (render [_]
+      (print app)
       (dom/div #js {:className "container"}
         (om/build title-field app)
         (om/build left-menu app)
