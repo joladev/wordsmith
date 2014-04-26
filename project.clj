bc85d7e6636a40a9515246d773e0a328e933ad1b
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-04-26 12:24:55 +0200

    Refactors project structure

--- project.clj
+++ project.clj
@@ -1,22 +1,25 @@
-(defproject wordsmith "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+(defproject wordsmith "0.0.1"
+  :description "A markdown editor with live preview and localStorage
+                saving. Built using ClojureScript and Om."
+  :url "https://github.com/eakron/wordsmith"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.6.0"]
-                 [com.facebook/react "0.8.0.1"]]
+                 [om "0.6.2"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
   :cljsbuild { 
-    :builds [{:id "wordsmith"
+    :builds [{:id "release"
               :source-paths ["src"]
               :compiler {
-                :output-to "wordsmith.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true}}]})
+                :output-to "resources/main.js"
+                :optimizations :advanced
+                :pretty-print false
+                :preamble ["react/react.min.js"]
+                :externs ["react/externs/react.js"
+                          "marked.js"]
+                :closure-warnings {:externs-validation :off}}}]})
